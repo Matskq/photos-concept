61dbb27f11bc0952b5c57f64112a0af80d91b18a
Matias Mattila <42553354+Matskq@users.noreply.github.com>
2021-03-19 18:17:30 +0200

    js

---
+++ AYC6YE2QA7/SJ7858SVPC.js
@@ -0,0 +1,98 @@
+function opnLine() {
+    /*// BG //*/
+    document.body.style.backgroundImage = "url(../U4FC36S2RX/2021/400.jpg)";
+
+    /*// TAB //*/
+    document.getElementById("Z81GT").style.display = "block";
+    document.getElementById("UFA59").style.display = "none";
+    document.getElementById("CAU1E").style.display = "none";
+    document.getElementById("L9XW6").style.display = "none";
+
+    /*// ICON //*/
+    document.getElementById("Z81GT-1").style.display = "block";
+    document.getElementById("Z81GT-2").style.display = "none";
+
+    document.getElementById("UFA59-1").style.display = "none";
+    document.getElementById("UFA59-2").style.display = "block";
+
+    document.getElementById("CAU1E-1").style.display = "none";
+    document.getElementById("CAU1E-2").style.display = "block";
+    
+    document.getElementById("L9XW6-2").style.display = "block";
+    document.getElementById("L9XW6-1").style.display = "none";
+}
+
+function opnLib() {
+    /*// BG //*/
+    document.body.style.backgroundImage = "url(../U4FC36S2RX/2020/01.jpg)";
+
+    /*// TAB //*/
+    document.getElementById("Z81GT").style.display = "none";
+    document.getElementById("UFA59").style.display = "block";
+    document.getElementById("CAU1E").style.display = "none";
+    document.getElementById("L9XW6").style.display = "none";
+
+    /*// ICON //*/
+    document.getElementById("Z81GT-1").style.display = "none";
+    document.getElementById("Z81GT-2").style.display = "block";
+
+    document.getElementById("UFA59-1").style.display = "block";
+    document.getElementById("UFA59-2").style.display = "none";
+
+    document.getElementById("CAU1E-1").style.display = "none";
+    document.getElementById("CAU1E-2").style.display = "block";
+    
+    document.getElementById("L9XW6-2").style.display = "block";
+    document.getElementById("L9XW6-1").style.display = "none";
+}
+
+function opnOsu() {
+
+    /*// TAB //*/
+    document.getElementById("Z81GT").style.display = "none";
+    document.getElementById("UFA59").style.display = "none";
+    document.getElementById("CAU1E").style.display = "block";
+    document.getElementById("L9XW6").style.display = "none";
+
+    /*// ICON //*/
+    document.getElementById("Z81GT-1").style.display = "none";
+    document.getElementById("Z81GT-2").style.display = "block";
+
+    document.getElementById("UFA59-1").style.display = "none";
+    document.getElementById("UFA59-2").style.display = "block";
+
+    document.getElementById("CAU1E-1").style.display = "block";
+    document.getElementById("CAU1E-2").style.display = "none";
+    
+    document.getElementById("L9XW6-2").style.display = "block";
+    document.getElementById("L9XW6-1").style.display = "none";
+}
+
+function opnSet() {
+
+    /*// TAB //*/
+    document.getElementById("Z81GT").style.display = "none";
+    document.getElementById("UFA59").style.display = "none";
+    document.getElementById("CAU1E").style.display = "none";
+    document.getElementById("L9XW6").style.display = "block";
+
+    /*// ICON //*/
+    document.getElementById("Z81GT-1").style.display = "none";
+    document.getElementById("Z81GT-2").style.display = "block";
+
+    document.getElementById("UFA59-1").style.display = "none";
+    document.getElementById("UFA59-2").style.display = "block";
+
+    document.getElementById("CAU1E-1").style.display = "none";
+    document.getElementById("CAU1E-2").style.display = "block";
+
+    document.getElementById("L9XW6-2").style.display = "none";
+    document.getElementById("L9XW6-1").style.display = "block";
+}
+
+function clsAll() {
+
+    /*// あ //*/
+    document.getElementById("V8COJ").style.display = "none";
+    document.getElementById("K3RCX").style.display = "block";
+}
